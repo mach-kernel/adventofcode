383b2d831ff81b85ced5f0673debe89dd9411dc3
David Stancu <dstancu@barracuda.com>
2021-12-01 11:42:49 -0500

    day 1 pt 2

--- 2021/src/advent/core.clj
+++ 2021/src/advent/core.clj
@@ -4,13 +4,13 @@
             [advent.days.one]))
 
 (defn -main
-  [& [day]]
+  [& [day run-fn]]
   (when (empty? day)
     (println "Usage: advent [day-ns-name]")
     (System/exit 1))
   (when-let [f (-> day
                    (s/trim)
-                   ((partial format "advent.days.%s/solve"))
+                   (#(format "advent.days.%s/%s" % (or run-fn "solve")))
                    (symbol)
                    (resolve))]
     (println (format "Day %s:\n%s" day (f)))))
--- 2021/src/advent/days/one.clj
+++ 2021/src/advent/days/one.clj
@@ -5,9 +5,9 @@
 ; Read input from JAR rsrc into int-seq
 (def input
   (->> "day_1.txt"
-       (io/resource)
-       (slurp)
-       (s/split-lines)
+       io/resource
+       slurp
+       s/split-lines
        (map #(Integer/parseInt %))))
 
 ; O(N) time, O(1) space (excl. seq)
@@ -21,3 +21,17 @@
         [x ct]))
     [(Integer/MAX_VALUE) 0]
     input))
+
+(defn solve-pt-2
+  "Counts number of monotonic sliding windows"
+  []
+  (loop [v (vec input)
+         s 0
+         f 3
+         prev (Integer/MAX_VALUE)
+         ct 0]
+    (let [subsum (reduce + (subvec v s f))
+          ct (if (> subsum prev) (inc ct) ct)]
+      (if (< f (count v))
+        (recur v (inc s) (inc f) subsum ct)
+        ct))))
