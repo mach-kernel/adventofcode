d1f6bc6618dbe27f82e106f58bb0360df162a671
David Stancu <dstancu@barracuda.com>
2023-12-02 00:54:06 -0500

    cleanup

--- 2023/src/advent23/d02.clj
+++ 2023/src/advent23/d02.clj
@@ -1,17 +1,6 @@
 (ns advent23.d02
   (:require [clojure.string :as str]))
 
-(defn -main
-  [& argv]
-  (println argv))
-
-(comment
-  "the game
-   
-   cubes: red/green/blue
-   hides rand cubes in bag
-   ")
-
 (defn read-game
   [s]
   (letfn [(play->pairs
@@ -31,28 +20,28 @@
                      (map play->pairs))]
       [game tapes])))
 
-(comment
-  ;; part 1
-  (let [input (slurp "resources/d02.txt")
-        games (->> input
-                   str/split-lines
-                   (map read-game))
-        valid-game? (fn [{:keys [red green blue] :or {red 0 green 0 blue 0}}] 
-                      (and (<= red 12)
-                           (<= green 13)
-                           (<= blue 14)))]
-    (->> games
-         (filter #(every? valid-game? (last %)))
-         (map first)
-         (reduce +)))
-  
-  ;; part 2
-  (let [input (slurp "resources/d02.txt")
-        games (->> input
-                   str/split-lines
-                   (map read-game))]
-    (->> games
-         (map #(apply merge-with max (last %)))
-         (map #(reduce * (vals %)))
-         (reduce +))))
+(defn part-1
+  [s]
+  (letfn [(valid-game? [{:keys [red green blue] :or {red 0 green 0 blue 0}}]
+           (and (<= red 12)
+                (<= green 13)
+                (<= blue 14)))]
+   (->> s
+        str/split-lines
+        (map read-game)
+        (filter #(every? valid-game? (last %)))
+        (map first)
+        (reduce +))))
 
+(defn part-2
+  [s]
+  (->> s
+       str/split-lines
+       (map read-game)
+       (map #(apply merge-with max (last %)))
+       (map #(reduce * (vals %)))
+       (reduce +)))
+
+(comment
+  (part-1 (slurp "resources/d02.txt"))
+  (part-2 (slurp "resources/d02.txt")))
