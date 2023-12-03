cff84130ada6e20fd7dfe0f306958e25d2eb0749
David Stancu <dstancu@barracuda.com>
2023-12-03 16:51:40 -0500

    clarity/cleanup

--- 2023/src/advent23/d03.clj
+++ 2023/src/advent23/d03.clj
@@ -1,7 +1,6 @@
 (ns advent23.d03
-  (:require
-    [clojure.set :as set]
-    [clojure.string :as str]))
+  (:require [clojure.set :as set]
+            [clojure.string :as str]))
 
 (defn crawl-island
   "Beginning from an anchor token, flood-fill island coordinates"
@@ -37,31 +36,23 @@
               [i x]))]
     (vec (map-indexed labeler coll))))
 
-(defn token-coords
-  "Given a matrix, emit a sequence of token coordinates"
-  [matrix]
-  (for [[y row] (deep-indexed matrix)
-        [x [^Character char]] row
-        :when (not (or (Character/isDigit char)
-                       (= char \.)))]
-    [y x]))
-
-(defn gear-coords
+(defn find-coords
   "Given a matrix, emit a sequence of gear coordinates"
-  [matrix]
+  [pred matrix]
   (for [[y row] (deep-indexed matrix)
         [x [^Character char]] row
-        :when (= char \*)]
+        :when (pred char)]
     [y x]))
 
 (defn part-1
-  "1. Tokenize into matrix
-   2. Collect coordinates of token connected islands.
-   3. Mask out non island coordinates
-   4. Regex out remaining numbers and reduce into sum"
+  "1. Collect coordinates of token connected islands.
+   2. Mask out non island coordinates
+   3. Regex out remaining numbers and reduce into sum"
   [s]
   (let [matrix (mapv #(str/split % #"") (str/split-lines s))
-        valid-mask (->> (token-coords matrix)
+        token-pred #(not (or (Character/isDigit ^Character %)
+                             (= % \.)))
+        valid-mask (->> (find-coords token-pred matrix)
                         (map (partial crawl-island matrix))
                         (reduce set/union))
         nums-str (apply str (for [[y row] (deep-indexed matrix)
@@ -69,12 +60,13 @@
                               (if (contains? valid-mask [y x])
                                 ch
                                 ",")))]
-    [nums-str
-     (->> (re-seq #"\d+" nums-str)
-          (map parse-long)
-          (reduce +))]))
+    [nums-str (->> (re-seq #"\d+" nums-str)
+                   (map parse-long)
+                   (reduce +))]))
 
 (defn line->nums-islands
+  "Given a map of coords->island label and an input line,
+  return a seq of [[num label] ...]"
   [g->l y line]
   (let [num-matcher (re-matcher #"\d+" line)]
     (for [[n start] (->> (repeatedly #(let [num (re-find num-matcher)]
@@ -86,15 +78,14 @@
       [n island-label])))
 
 (defn part-2
-  "1. Tokenize into matrix
-   2. Label *-connected islands in order they are discovered
-   3. Find each number and annotate with its island
-   4. Keep all pairs with 2 islands, get their product
-   5. Reduce into final sum"
+  "1. Label *-connected islands in order they are discovered
+   2. Find each number and annotate with its island
+   3. Keep all pairs with 2 islands, get their product
+   4. Reduce into final sum"
   [s]
-  (let [matrix (mapv #(str/split % #"") (str/split-lines s))
-        lines (str/split-lines s)
-        islands (->> (gear-coords matrix)
+  (let [lines (str/split-lines s)
+        matrix (mapv #(str/split % #"") lines)
+        islands (->> (find-coords #(= % \*) matrix)
                      (map (partial crawl-island matrix)))
         gear-coords->label (->> islands
                                 (map-indexed #(zipmap %2 (repeat %1)))
@@ -105,7 +96,7 @@
                           (group-by last))]
     (->> (vals nums->island)
          (keep #(when (= (count %) 2)
-                  (->> % (map (comp parse-long first)))))
+                  (map (comp parse-long first) %)))
          (reduce #(+ %1 (apply * %2)) 0))))
 
 (comment
